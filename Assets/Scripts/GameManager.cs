818303b8df52c7f32592b775cf4d6c3fffa2981f
AkioMabuchi <mabuchiakio@gmail.com>
2020-11-24 06:35:31 +0900

    first commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+
+public class GameManager : SingletonMonoBehaviour<GameManager>
+{
+    [SerializeField] private TimerCounter _timerCounter;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _timerCounter.OnTimeChanged.Subscribe(time =>
+        {
+            Debug.Log(time);
+        });
+    }
+}
---
+++ Assets/Scripts/TimerCounter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class TimerCounter : MonoBehaviour
+{
+    private Subject<int> _timerSubject = new Subject<int>();
+
+    public IObservable<int> OnTimeChanged
+    {
+        get
+        {
+            return _timerSubject;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(CoroutineTimer());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator CoroutineTimer()
+    {
+        int time = 100;
+        while (time > 0)
+        {
+            time--;
+            _timerSubject.OnNext(time);
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+}
